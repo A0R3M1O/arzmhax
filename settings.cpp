e8a3b28449d2fdc5016c800a33206c57ff381ac0
kefs <32211521+Codeesar@users.noreply.github.com>
2020-02-10 16:09:54 +1000

    class init fix

--- settings.cpp
+++ settings.cpp
@@ -6,13 +6,6 @@ CSettings::CSettings()
 {	
 	LOGI("CSettings init..");
 
-	if(!g_pszStorage)
-	{
-		g_pszStorage = (const char*)(g_libGTASA + 0x006D687C);
-
-		if(!g_pszStorage) return;
-	}
-
 	char buff[0x7F];
 	sprintf(buff, "%sSAMP/sets.ini", g_pszStorage);
 
